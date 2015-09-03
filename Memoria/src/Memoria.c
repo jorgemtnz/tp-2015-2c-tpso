80a6c456420c6d6eecdd08799fadf8e51f205513
MatiNav <matiasnavarroezequiel@gmail.com>
2015-09-03 00:07:59 -0300

    listo memoria

--- Memoria/src/Memoria.c
+++ Memoria/src/Memoria.c
@@ -2,9 +2,10 @@
 
 #include "Memoria.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	printf("a\n");
 	logger = log_create("LOG_Memoria.log", "Memoria", false, LOG_LEVEL_INFO); //Inicializacion logger
-	leerArchivoDeConfiguracion();
+	leerArchivoDeConfiguracion(argc,argv);
 	printf("a\n");
 
 	return EXIT_SUCCESS;
--- Memoria/src/funciones_memoria.c
+++ Memoria/src/funciones_memoria.c
@@ -1,22 +1,29 @@
 #include "Memoria.h"
 //escribir las funciones aqui
 
-void leerArchivoDeConfiguracion() {
-	char* nombreArchivoConfig;
-	t_config* archivoConfig = NULL;
-	int result, fd_archivo = 0;
-	result = 0;
+void leerArchivoDeConfiguracion(int argc, char *argv[]) {
 
-	nombreArchivoConfig = strdup("/home/utnso/tp-2015-2c-tpso/Memoria/config_memoria.cfg");
-	result = checkearRutaArchivoConfig(nombreArchivoConfig);
+	char* logMsg = NULL;
+
+	if (argc < 2) {
+		logMsg =
+				string_from_format(
+						"Debe especificar la ruta al archivo de configuracion, al invocar al programa, por ejemplo: ./Memoria /home/utnso/tp-2015-2c-tpso/Memoria/config_memoria.cfg\n");
+		puts(logMsg);
+		log_error(logger, logMsg);
+		exit(-1);
+	}
+
+	char* nombreArchivoConfig = nombreArchivoConfig = strdup(argv[1]);
+	int result = checkearRutaArchivoConfig(nombreArchivoConfig);
 	if (result == -1) {
-		perror("[ERROR]: Archivo de configuracion no encontrado");
-		log_error(logger, "[ERROR]: Archivo de configuracion no encontrado");
+		logMsg = string_from_format("Archivo de configuracion no encontrado. Parametro especificado: %s\n", nombreArchivoConfig);
+		puts(logMsg);
+		log_error(logger, logMsg);
 		exit(-1);
 	} else {
-
+		t_config* archivoConfig;
 		archivoConfig = config_create(nombreArchivoConfig);
-
 		configuracion = malloc(sizeof(t_configuracion));
 		configuracion->puertoEscucha = config_get_int_value(archivoConfig, "PUERTO_ESCUCHA");
 		configuracion->puertoSwap = config_get_int_value(archivoConfig, "PUERTO_SWAP");
@@ -29,7 +36,28 @@ void leerArchivoDeConfiguracion() {
 		configuracion->retardoMemoria = config_get_int_value(archivoConfig, "RETARDO_MEMORIA");
 		configuracion->tlbHabilitada = config_get_int_value(archivoConfig, "TLB_HABILITADA");
 		log_info(logger, "[INFO]: Archivo de configuracion leido correctamente");
+
+		logMsg = string_from_format("Archivo de configuracion leido correctamente\n");
+		puts(logMsg);
+		log_error(logger, logMsg);
+
+		config_destroy(archivoConfig);
 	}
-	config_destroy(archivoConfig);
-	close(fd_archivo);
+
 }
+
+
+/*
+configuracion = malloc(sizeof(t_configuracion));
+		configuracion->puertoEscucha = config_get_int_value(archivoConfig, "PUERTO_ESCUCHA");
+		configuracion->puertoSwap = config_get_int_value(archivoConfig, "PUERTO_SWAP");
+		configuracion->ipSwap = strdup(config_get_string_value(archivoConfig, "IP_SWAP"));
+		configuracion->nombreMemoria = strdup(config_get_string_value(archivoConfig, "NOMBRE_MEMORIA"));
+		configuracion->maximosMarcosPorProceso = config_get_int_value(archivoConfig, "MAXIMO_MARCOS_POR_PROCESO");
+		configuracion->cantidadMarcos = config_get_int_value(archivoConfig, "CANTIDAD_MARCOS");
+		configuracion->tamanioMarcos = config_get_int_value(archivoConfig, "TAMANIO_MARCO");
+		configuracion->entradasTlb = config_get_int_value(archivoConfig, "ENTRADAS_TLB");
+		configuracion->retardoMemoria = config_get_int_value(archivoConfig, "RETARDO_MEMORIA");
+		configuracion->tlbHabilitada = config_get_int_value(archivoConfig, "TLB_HABILITADA");
+		log_info(logger, "[INFO]: Archivo de configuracion leido correctamente");
+*/
