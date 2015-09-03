4185e2ff5a13bcde9aeabeab6505d86f02ddfff0
Alejandro Zalazar <alejandrozalazar@gmail.com>
2015-09-03 00:30:29 -0300

    Se conecta al planificador y memoria y queda en escucha

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -1,13 +1,18 @@
-
 #include "CPU.h"
-
 
 int main(void) {
 
-	 logger = log_create("LOG_CPU.log", "CPU", false, LOG_LEVEL_INFO); //Inicializacion logger
-	 leerArchivoDeConfiguracion();
-     printf("ip leida %s \n", configuracion->vg_ipMemoria);
+	logger = log_create("LOG_CPU.log", "CPU", false, LOG_LEVEL_INFO); //Inicializacion logger
+	leerArchivoDeConfiguracion();
+	printf("ip leida %s \n", configuracion->vg_ipMemoria);
 
+	int socketPlanificador;
+	int socketMemoria;
+
+	conectar(configuracion->vg_ipPlanificador, string_itoa(configuracion->vg_puertoPlanificador), &socketPlanificador);
+	conectar(configuracion->vg_ipMemoria, string_itoa(configuracion->vg_puertoMemoria), &socketMemoria);
+
+	escucharConexiones("0", socketPlanificador, socketMemoria, 0);
 
 	return EXIT_SUCCESS;
 }
