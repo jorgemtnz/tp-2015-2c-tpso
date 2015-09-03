7086409fae790929749df094139d49d9e19836cd
Alejandro Zalazar <alejandrozalazar@gmail.com>
2015-09-03 00:29:26 -0300

    Se escuchan conexiones de CPU y se conecta a Swap

--- Memoria/src/Memoria.c
+++ Memoria/src/Memoria.c
@@ -1,12 +1,14 @@
-
-
 #include "Memoria.h"
 
 int main(int argc, char *argv[]) {
 	printf("a\n");
 	logger = log_create("LOG_Memoria.log", "Memoria", false, LOG_LEVEL_INFO); //Inicializacion logger
-	leerArchivoDeConfiguracion(argc,argv);
+	leerArchivoDeConfiguracion(argc, argv);
 	printf("a\n");
+
+	int socketSwap;
+	conectar(configuracion->ipSwap, string_itoa(configuracion->puertoSwap), &socketSwap);
+	escucharConexiones(string_itoa(configuracion->puertoEscucha), 0, 0, socketSwap);
 
 	return EXIT_SUCCESS;
 }
