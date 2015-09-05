78a8ec03461ac22432f55cbbfb7bfaf60051071b
Alejandro Zalazar <alejandrozalazar@gmail.com>
2015-09-05 19:50:10 -0300

    Se agrega manejo de mensajes del Planificador

--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -1,10 +1,13 @@
 #include "Planificador.h"
 
+t_dictionary* conexiones;
 int main(int argc, char *argv[]) {
+	conexiones = dictionary_create();
+
 	puts("Iniciando programa");
 	crearLogger();
 	leerArchivoDeConfiguracion(argc, argv);
-	escucharConexiones(configuracion->puertoEscucha, 0, 0, 0);
+	escucharConexiones(configuracion->puertoEscucha, 0, 0, 0, procesarMensajes, NULL, logger);
 	levantarConsola();
 
 	puts("Programa finalizado");
@@ -13,4 +16,26 @@ int main(int argc, char *argv[]) {
 
 void crearLogger() {
 	logger = log_create("LOG_PLAN.log", "Planificador", false, LOG_LEVEL_INFO);
+}
+
+
+int procesarMensajes(int socket, char* buffer, bool nuevaConexion, void* extra, t_log* logger) {
+	puts("Planificador procesar mensajes");
+	defaultProcesarMensajes(socket, buffer, nuevaConexion, extra, logger);
+	if(nuevaConexion) {
+		dictionary_put(conexiones, "CPU", string_itoa(socket));
+	} else {
+		if(socket == STDIN_FILENO && buffer != NULL) {
+			char** split = string_split(buffer, "\n");
+			buffer = split[0];
+		} else {
+			printf("Recibi el mensaje: %s\n", buffer);
+		}
+		if(string_starts_with(buffer, "correr programa")) {
+			char* socketCPU = (char*)dictionary_get(conexiones, "CPU");
+			puts("Enviando \"correr programa\" a la CPU");
+			enviar(atoi(socketCPU), "correr programa", strlen("correr programa"));
+		}
+	}
+	return 0;
 }
