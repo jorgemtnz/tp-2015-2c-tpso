aced879d9b9efb03845c66ae11f95caa7ea6c7db
jorgemtnz <jorgeemtnz@gmail.com>
2015-09-13 11:36:35 -0300

    se agregan tokens y funciones de manejo del paquete que se envia por el socket, se agrega la estructura del paquete

--- utiles/utiles/protocolo.c
+++ utiles/utiles/protocolo.c
@@ -1,3 +1,30 @@
 #include "protocolo.h"
 
-//funciones para el manejo del protocolo
+//+++++++++++++++++++++++++funciones para el manejo del protocolo++++++++++++++++++++++++++++++++++
+
+//objetivo devolver el paquete para el envio por socket
+
+PaqueteEnvio* serializar(int tipoDeMensaje, void* payLoad, int tamanioPayLoad){
+	PaqueteEnvio* paqueteAEnviar = malloc(sizeof(PaqueteEnvio));
+	size_t tamanioDelMensaje = tamanioPayLoad + sizeof(int); //+4 por el total del mensaje, le sumo un sizeof(int) por el header
+
+	paqueteAEnviar->mensaje = malloc(tamanioDelMensaje);
+	memcpy(paqueteAEnviar->mensaje,&tipoDeMensaje,sizeof(int));
+	size_t desplazameinto = sizeof(int);
+	memcpy(paqueteAEnviar->mensaje + desplazameinto,payLoad,tamanioPayLoad);
+
+	paqueteAEnviar->tamanioMensaje = tamanioDelMensaje;
+	return paqueteAEnviar;
+}
+
+// devolver el paquete para recepcion( y luego interpretar el paquete, referido al switch con los tokens)
+Paquete* deserializar(void* buffer, int tamnioMensaje) {
+	Paquete* paqueteDeserial = malloc(sizeof(Paquete));
+	memcpy(&(paqueteDeserial->tipoDeMensaje),buffer,sizeof(int));
+	int dezplazamiento = sizeof(int);
+	size_t tamanioALeerDelBuffer = tamnioMensaje - dezplazamiento;
+	paqueteDeserial->payLoad = malloc(tamanioALeerDelBuffer);
+	memcpy(paqueteDeserial->payLoad, buffer + dezplazamiento,tamanioALeerDelBuffer);
+	return paqueteDeserial;
+}
+
--- utiles/utiles/protocolo.h
+++ utiles/utiles/protocolo.h
@@ -7,16 +7,43 @@
 
 #ifndef UTILES_PROTOCOLO_H_
 #define UTILES_PROTOCOLO_H_
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++ INCLUDE   ++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#define CONTEXTO_MPROC 1
-#define R_INSTRUC  2
-#define I_MEMORIA 3
-#define L_MEMORIA 4
-#define E_MEMORIA 5
-#define F_INSTRUC_MEM 6
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++  DEFINE   +++++++++++++++++++++++++++++++++++++++++++++++++++
+#define CONTEXTO_MPROC  1
+#define RESUL_INSTR_EJEC  2
+#define INICIAR_PROC    3
+#define LEER            4
+#define ESCRIBIR        5
+#define FIN_PROCESO     6
+#define RESUL_OK        7
+#define RESUL_ERROR     8
+#define RESUL_INICIAR_PROC 9
+#define RESUL_ESCRIBIR  10
+#define RESUL_LEER      11
+#define RESUL_FIN       12
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++  ESTRUCTURAS   ++++++++++++++++++++++++++++++++++++++++
 
+typedef struct PaqueteEnvio{
+ uint8_t tamanioMensaje;
+	void* mensaje;
+ }PaqueteEnvio;
+
+typedef struct Paquete{
+	uint8_t tipoDeMensaje;
+	void* payLoad;
+}Paquete;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++ FUNCIONES  ++++++++++++++++++++++++++++++++++++++++++++++
+
+PaqueteEnvio* serializar(int tipoDeMensaje, void* payLoad, int tamanioPayLoad);
+Paquete* deserializar(void* buffer,int tamanioMensaje);
 
 #endif /* UTILES_PROTOCOLO_H_ */
